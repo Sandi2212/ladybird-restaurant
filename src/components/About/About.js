2fec3943388ac42a3c348d2e6f3f8c2d76727257
samuel fagbamigbeE <samuelfagbamigbe@gmail.com>
2019-02-15 18:13:08 -0500

    About-page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
-    return (
-        <div className="About" > 
+  return (
+    <div className="About">
+      <h1 className="about_heading">ABOUT</h1>
+      <img className="about_img" src="/assets/Bitmap/bitmap.png" alt="image" />
 
-        <h1 className="hours__heading">ABOUT</h1>
-      <img className="Ravi-logo" src="/assets/Bitmap/bitmap.png" alt="image" />
+      <div className="RaviDeRossi ">
+        <h2 className="raviderossi_title">Ravi DeRossi</h2>
+        <h3 className="raviderossi_restaur">Restauranteur</h3>
+        <p>
+          Ravi traveled around the world as an artist and painter, before
+          returning to his native New York City and taking the nightlife world
+          by storm. He created a mini-empire of world renowned cocktail lounges
+          and bars, including Death &amp; Company, The Bourgeois Pig,
+          Proletariat, Cienfuegos, Mayahuel, Amor y Amargo and 124 Rabbit Club.
+          He then established himself as a rising star on the culinary scene
+          with the acclaimed seafood hotspots Desnuda and Bergen Hill. Mother of
+          Pearl brings together these two worlds and is his first hybrid
+          restaurant and cocktail bar. Avant garden, an upscale vegan
+          restaurant. A lifelong animal lover, ravi has launched non-profit
+          beast, which will hold fundraising parties to improve animal lives.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-        <div className="Ravi DeRossi Restaur"> 
-        <h2 className="ravi-title">Ravi DeRossi Restauranteur</h2>
-        </div>
-
-        
-        
-        </div>
-    );
-}
-
-export default About 
+export default About;
