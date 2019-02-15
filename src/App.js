1b45e014407299415e44b39d54fef45786149565
Brian Ogilvie <brian@brianogilviedesigns.com>
2019-02-15 09:59:53 -0500

    created Home component with content, no CSS yet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Home from './components/Home/Home'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         Ladybird
+        <Home />
       </div>
     );
   }
