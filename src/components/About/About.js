7eb89b130f4ec93f97ca8b480c593bcc6c617264
samuel fagbamigbeE <samuelfagbamigbe@gmail.com>
2019-02-15 15:11:17 -0500

    Reserve Button

---
+++ src/components/About/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="About" > 
+
+        <h1 className="hours__heading">ABOUT</h1>
+      <img className="Ravi-logo" src="/assets/Bitmap/bitmap.png" alt="image" />
+
+        <div className="Ravi DeRossi Restaur"> 
+        <h2 className="ravi-title">Ravi DeRossi Restauranteur</h2>
+        </div>
+
+        
+        
+        </div>
+    );
+}
+
+export default About 
---
+++ src/components/Button/ReserveButton.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Button = () => {
+     return (
+     <button>
+     <a className="ReserveButton" href="https://resy.com/cities/ny/ladybird?gclid=EAIaIQobChMI9577u6K-4AIVGI7ICh3ljgTsEAAYASAAEgLPr_D_BwE&seats=2&date=2019-02-15" target="_blank" rel="noopener noreferrer">
+    Book Reservation
+    </a>
+     </button>
+    
+     );
+};
+
+export default Button 
+
+
+
