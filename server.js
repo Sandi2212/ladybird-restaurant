9f2421d31001d09ede9f865023c2573c537679c9
sdiperi17 <sdiperi17@gmail.com>
2019-02-15 15:29:21 -0500

    installed passport, created config folder

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
-const express = require('express')
-const PORT = process.env.PORT || 5000
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
+const express = require("express");
+const PORT = process.env.PORT || 5000;
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const app = express();
+const passport = require("passport");
+const GooglesStrategy = require("passport-google-oauth20").Strategy;
 
-const app = express()
-app.use(bodyParser.json())
-app.use(morgan('dev'))
+// console.developers.google.com
+// clientID 202709015824-6hvrvmbj9jg44lhs9m1ao8n63qo5njg4.apps.googleusercontent.com
+// clientSecret 6hzjX8uvYDDk73e6MQl5i0Zu
+passport.use(new GoogleStrategy());
+app.use(bodyParser.json());
+app.use(morgan("dev"));
 
-app.get('/', (req, res) => {
-  res.send("Welcome to Ladybird")
-})
+app.get("/", (req, res) => {
+    res.send("Welcome to Ladybird");
+});
 
 app.listen(PORT, () => {
-  console.log(`Express server is listening on port ${PORT}`)
-})
+    console.log(`Express server is listening on port ${PORT}`);
+});
