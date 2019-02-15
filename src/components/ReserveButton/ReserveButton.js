b57dde620d6f5b93553f07579e29c6528a4fadae
samuel fagbamigbeE <samuelfagbamigbe@gmail.com>
2019-02-15 18:00:27 -0500

    ReserveButton new update

--- src/components/ReserveButton/ReserveButton.js
+++ src/components/ReserveButton/ReserveButton.js
@@ -4,7 +4,7 @@ const ReserveButton = () => {
     return ( 
     <button className = "ReserveButton"
         href = "https://resy.com/cities/ny/ladybird?gclid=EAIaIQobChMI9577u6K-4AIVGI7ICh3ljgTsEAAYASAAEgLPr_D_BwE&seats=2&date=2019-02-15">
-        Book Reservation </button> 
+        Book Reservation</button> 
     );
 };
 
