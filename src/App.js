e4d13dfb98789ed71bd7dbf041bcdc7952edc80e
Brian Ogilvie <brian@brianogilviedesigns.com>
2019-02-15 09:28:07 -0500

    created basic skeleton for page layout with placeholders for all components

--- src/App.js
+++ src/App.js
@@ -5,7 +5,35 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        Ladybird
+        {/* <Header /> */}
+        <main>
+          <div className="page-section">
+            {/* <Home /> */}
+            <div className="sticky-side"></div>
+          </div>
+          <div className="page-section">
+            {/* <Blurb /> */}
+            <div className="sticky-side"></div>
+          </div>
+          <div className="page-section">
+            <div className="sticky-side"></div>
+            {/* <Menus /> */}
+          </div>
+          <div className="page-section">
+            {/* <About /> */}
+            <div className="sticky-side"></div>
+          </div>
+          <div className="full-screen-image"></div>
+          <div className="page-section">
+            <div className="sticky-side"></div>
+            {/* <Events /> */}
+          </div>
+          <div className="page-section">
+            {/* <Contact /> */}
+            <div className="sticky-side"></div>
+          </div>
+        </main>
+        {/* <Footer /> */}
       </div>
     );
   }
