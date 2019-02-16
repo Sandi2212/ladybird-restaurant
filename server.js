4f58b5795cf04a977523e5a6ef39cc64800d59df
OneilCampbell <ocampbell@colgate.edu>
2019-02-16 12:47:51 -0500

    wrote routes to create, update, and delete food item listings

--- server.js
+++ server.js
@@ -191,6 +191,44 @@ app.get('/foods', async (req, res) => {
   }
 })
 
+app.post('/foods', async (req, res) => {
+  try {
+    await Food.create(req.body);
+    res.send(true);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: error.message
+    });
+  }
+});
+
+app.put('foods/:id', async (req, res) => {
+  try {
+    const foodToUpdate = await Food.findByPk(req.params.id);
+    await foodToUpdate.update(req.body);
+    res.send(true);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: error.message
+    });
+  }
+});
+
+app.delete('foods/:id', async (req, res) => {
+  try {
+    const foodToRemove = await Food.findByPk(req.params.id);
+    await foodToRemove.destroy();
+    res.send(true);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: error.message
+    });
+  }
+});
+
 // ----------------------------------------
 //  *********** FONDUE ROUTES ************
 // ----------------------------------------
