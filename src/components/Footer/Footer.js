9440752c760ad2c7dba78ae3bd0eb8fac1dcc6ad
Brian Ogilvie <brian@brianogilviedesigns.com>
2019-02-16 14:17:06 -0500

    populated Footer component. no social links yet

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import './Footer.css'
+import MailChimp from '../Mailchimp/MailChimp'
 
 const Footer = props => {
   return (
-    <div className="Footer">Footer</div>
+    <div className="Footer">
+      <div className="footer__links">
+        <ul className="footer__nav">
+          <li className="footer__nav-item"><a href="#">Home</a></li>
+          <li className="footer__nav-item"><a href="#">Menus</a></li>
+          <li className="footer__nav-item"><a href="#">Gift Cards</a></li>
+          <li className="footer__nav-item"><a href="#">Events</a></li>
+          <li className="footer__nav-item"><a href="#">Contact</a></li>
+        </ul>
+      </div>
+      <MailChimp />
+    </div>
   )
 }
 
