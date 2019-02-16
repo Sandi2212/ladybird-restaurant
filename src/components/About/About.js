c4528049925ce65e8b9f9cf3d0133c13c8ef64c1
samuel fagbamigbeE <samuelfagbamigbe@gmail.com>
2019-02-15 19:32:01 -0500

    About Comp

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,20 +1,24 @@
 import React from "react";
+import './About.css'
 
 const About = () => {
   return (
     <div className="About">
       <h1 className="about_heading">ABOUT</h1>
-      <img className="about_img" src="/assets/Bitmap/bitmap.png" alt="image" />
-
-      <div className="RaviDeRossi ">
-        <h2 className="raviderossi_title">Ravi DeRossi</h2>
-        <h3 className="raviderossi_restaur">Restauranteur</h3>
+      <div className="about_img">
+      <img src="/assets/Ravi/derossi.png" alt="Ravi DeRossi" />
+        </div>  
+      <div className="Ravi-DeRossi ">
+        <h2 className="Ravi-DeRossi_title">Ravi DeRossi</h2>
+        <h3 className="Ravi-DeRossi_restaur">Restauranteur</h3>
+        <div className="Ravi-DeRossi_travel">
         <p>
           Ravi traveled around the world as an artist and painter, before
           returning to his native New York City and taking the nightlife world
           by storm. He created a mini-empire of world renowned cocktail lounges
           and bars, including Death &amp; Company, The Bourgeois Pig,
           Proletariat, Cienfuegos, Mayahuel, Amor y Amargo and 124 Rabbit Club.
+
           He then established himself as a rising star on the culinary scene
           with the acclaimed seafood hotspots Desnuda and Bergen Hill. Mother of
           Pearl brings together these two worlds and is his first hybrid
@@ -22,6 +26,8 @@ const About = () => {
           restaurant. A lifelong animal lover, ravi has launched non-profit
           beast, which will hold fundraising parties to improve animal lives.
         </p>
+        </div>
+    
       </div>
     </div>
   );
