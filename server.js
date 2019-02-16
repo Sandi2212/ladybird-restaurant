462d6e1a1f4b4c6dd2c84d11a947a8b4723f8921
OneilCampbell <ocampbell@colgate.edu>
2019-02-16 12:45:59 -0500

    wrote routes to create, update, and delete beer listings

--- server.js
+++ server.js
@@ -83,6 +83,44 @@ app.get('/beers', async (req, res) => {
   }
 })
 
+app.post('/beers', async (req, res) => {
+  try {
+    await Beer.create(req.body);
+    res.send(true);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: error.message
+    });
+  }
+});
+
+app.put('beers/:id', async (req, res) => {
+  try {
+    const beerToUpdate = await Beer.findByPk(req.params.id);
+    await beerToUpdate.update(req.body);
+    res.send(true);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: error.message
+    });
+  }
+});
+
+app.delete('beers/:id', async (req, res) => {
+  try {
+    const beerToRemove = await Beer.findByPk(req.params.id);
+    await beerToRemove.destroy();
+    res.send(true);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: error.message
+    });
+  }
+});
+
 // ----------------------------------------
 //  ********** COCKTAIL ROUTES ***********
 // ----------------------------------------
