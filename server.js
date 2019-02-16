b5812557acd62d64fb95e01ae94fc9929460efd7
OneilCampbell <ocampbell@colgate.edu>
2019-02-16 12:48:45 -0500

    wrote routes to create, update, and delete fondue listings

--- server.js
+++ server.js
@@ -245,6 +245,44 @@ app.get('/fondues', async (req, res) => {
   }
 })
 
+app.post('/fondues', async (req, res) => {
+  try {
+    await Fondue.create(req.body);
+    res.send(true);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: error.message
+    });
+  }
+});
+
+app.put('fondues/:id', async (req, res) => {
+  try {
+    const fondueToUpdate = await Fondue.findByPk(req.params.id);
+    await fondueToUpdate.update(req.body);
+    res.send(true);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: error.message
+    });
+  }
+});
+
+app.delete('fondues/:id', async (req, res) => {
+  try {
+    const fondueToRemove = await Fondue.findByPk(req.params.id);
+    await fondueToRemove.destroy();
+    res.send(true);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: error.message
+    });
+  }
+});
+
 // ----------------------------------------
 //  *********** DESSERT ROUTES ************
 // ----------------------------------------
