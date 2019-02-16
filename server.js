f8275d9c49a0797080d427613894edcfd717398a
OneilCampbell <ocampbell@colgate.edu>
2019-02-16 12:50:50 -0500

    wrote routes to create, update, and delete dessert listings

--- server.js
+++ server.js
@@ -299,6 +299,44 @@ app.get('/desserts', async (req, res) => {
   }
 })
 
+app.post('/desserts', async (req, res) => {
+  try {
+    await Dessert.create(req.body);
+    res.send(true);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: error.message
+    });
+  }
+});
+
+app.put('desserts/:id', async (req, res) => {
+  try {
+    const dessertToUpdate = await Dessert.findByPk(req.params.id);
+    await dessertToUpdate.update(req.body);
+    res.send(true);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: error.message
+    });
+  }
+});
+
+app.delete('desserts/:id', async (req, res) => {
+  try {
+    const dessertToRemove = await Dessert.findByPk(req.params.id);
+    await dessertToRemove.destroy();
+    res.send(true);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: error.message
+    });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Express server is listening on port ${PORT}`)
 })
