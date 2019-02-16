a73a414df51e4557becaa43980aa667b16527528
OneilCampbell <ocampbell@colgate.edu>
2019-02-16 12:44:03 -0500

    wrote routes to create, update, and delete wine listings

--- server.js
+++ server.js
@@ -29,6 +29,44 @@ app.get('/wines', async (req, res) => {
   }
 })
 
+app.post('/wines', async (req, res) => {
+  try {
+    await Wine.create(req.body);
+    res.send(true);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: error.message
+    });
+  }
+})
+
+app.put('wines/:id', async (req, res) => {
+  try {
+    const wineToUpdate = await Wine.findByPk(req.params.id);
+    await wineToUpdate.update(req.body);
+    res.send(true);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: error.message
+    });
+  }
+})
+
+app.delete('wines/:id', async (req, res) => {
+  try {
+    const wineToRemove = await Wine.findByPk(req.params.id);
+    await wineToRemove.destroy();
+    res.send(true);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: error.message
+    });
+  }
+})
+
 // ----------------------------------------
 //  ************ BEER ROUTES **************
 // ----------------------------------------
