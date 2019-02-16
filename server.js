7d22962ed7172b4ef8de54d829803188b7166a22
OneilCampbell <ocampbell@colgate.edu>
2019-02-16 12:46:49 -0500

    wrote routes to create, update, and delete cocktail listings

--- server.js
+++ server.js
@@ -137,6 +137,44 @@ app.get('/cocktails', async (req, res) => {
   }
 })
 
+app.post('/cocktails', async (req, res) => {
+  try {
+    await Cocktail.create(req.body);
+    res.send(true);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: error.message
+    });
+  }
+});
+
+app.put('cocktails/:id', async (req, res) => {
+  try {
+    const cocktailToUpdate = await Cocktail.findByPk(req.params.id);
+    await cocktailToUpdate.update(req.body);
+    res.send(true);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: error.message
+    });
+  }
+});
+
+app.delete('cocktails/:id', async (req, res) => {
+  try {
+    const cocktailToRemove = await Cocktail.findByPk(req.params.id);
+    await cocktailToRemove.destroy();
+    res.send(true);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: error.message
+    });
+  }
+});
+
 // ----------------------------------------
 //  ************* FOOD ROUTES *************
 // ----------------------------------------
