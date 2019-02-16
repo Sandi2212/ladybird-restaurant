07e2930e708604dacb9d1982ed17800ef27c2b41
OneilCampbell <ocampbell@colgate.edu>
2019-02-16 10:49:43 -0500

    wrote route to get info for all wines

--- server.js
+++ server.js
@@ -7,8 +7,22 @@ const app = express()
 app.use(bodyParser.json())
 app.use(morgan('dev'))
 
+const { Wine, Beer, Cocktail, Food, Fondue, Dessert } = require("./models/index")
+
 app.get('/', (req, res) => {
   res.send("Welcome to Ladybird")
+})
+
+app.get('/wine', async (req, res) => {
+  try {
+    const allWines = await Wine.findAll();
+    res.json(allWines);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: error.message
+    });
+  }
 })
 
 app.listen(PORT, () => {
