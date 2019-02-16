0c1362abe07ab2c710b0d78414f0b342d9cbcfc8
OneilCampbell <ocampbell@colgate.edu>
2019-02-16 11:00:08 -0500

    wrote route to get all info on beers

--- server.js
+++ server.js
@@ -13,10 +13,29 @@ app.get('/', (req, res) => {
   res.send("Welcome to Ladybird")
 })
 
+// ----------------------------------------
+//  ************ WINE ROUTES **************
+// ----------------------------------------
+
 app.get('/wine', async (req, res) => {
   try {
     const allWines = await Wine.findAll();
     res.json(allWines);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: error.message
+    });
+  }
+})
+// ----------------------------------------
+//  ************ BEER ROUTES **************
+// ----------------------------------------
+
+app.get('/beer', async (req, res) => {
+  try {
+    const allBeers = await Beer.findAll();
+    res.json(allBeers);
   } catch (error) {
     console.log(error);
     res.status(500).json({
