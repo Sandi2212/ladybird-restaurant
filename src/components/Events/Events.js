32b90626e18c8c63b2693df3ce90f670fe72236d
Brian Ogilvie <brian@brianogilviedesigns.com>
2019-02-15 20:15:46 -0500

    added all content to Events component. no styling yet

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Events.css'
+import ReserveButton from '../ReserveButton/ReserveButton'
 
 const Events = props => {
   return (
@@ -12,6 +13,19 @@ const Events = props => {
           Plus Tax & Gratuity<br />
           <span className="events__strong">6:30PM &amp; 9:00 PM</span>
         </p>
+      </div>
+      <div className="events__section">
+        <h4 className="events__section-heading">Non-existent Happy hour</h4>
+        <p className="events__section-content">
+          6-Course Tasting Menu<br />
+          $150 Per Couple<br />
+          Plus Tax & Gratuity<br />
+          <span className="events__strong">6:30PM & 9:00 PM</span>
+        </p>
+      </div>
+      <ReserveButton />
+      <div className="events__email">
+        Please email <a className="events__link" href="mailto:events@ladybirdny.com">events@ladybirdny.com</a> to reserve for large parties.
       </div>
     </div>
   )
