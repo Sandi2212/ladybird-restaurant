955ee6b89f7885ce2f7e503cfcedc7bc914dd56e
Brian Ogilvie <brian@brianogilviedesigns.com>
2019-02-15 19:40:56 -0500

    added some content to Events component

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -3,7 +3,17 @@ import './Events.css'
 
 const Events = props => {
   return (
-    <h1>Events</h1>
+    <div className="Events">
+      <h2 className="events__heading">Events</h2>
+      <div className="events__section">
+        <h4 className="events__section-heading">VALENTINE’S DAY</h4>
+        <p className="events__section-content">6-Course Tasting Menu<br />
+          $150 Per Couple<br />
+          Plus Tax & Gratuity<br />
+          <span className="events__strong">6:30PM &amp; 9:00 PM</span>
+        </p>
+      </div>
+    </div>
   )
 }
 
