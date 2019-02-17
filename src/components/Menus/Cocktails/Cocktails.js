d6586ba3dcd040bdb77251923613b4f170899c7d
Sandi <Sandiimayeguahi@gmail.com>
2019-02-17 01:04:24 -0500

    created class component for cocktails with axios call

--- src/components/Menus/Cocktails/Cocktails.js
+++ src/components/Menus/Cocktails/Cocktails.js
@@ -1,16 +1,35 @@
-import React from "react"
+import React , {Component} from "react"
+import axios from "axios"
+class  Cocktails extends Component{
+    constructor(){
+        super()
+        this.state={
+            allCocktails:null
+        
+    }
+}
 
-const Cocktails= () =>{
-
-
-return(
+getCocktailsData = async () => {
+    await axios.get("http://localhost:5000/cocktails").then(response => {
+     
+        const allCocktails = response.data;
+        this.setState({allCocktails});
+    });
+    }
+    componentDidMount(){
+        this.getCocktailsData();
+   
+     }
+  
+render(){
+    return(
 
 
     <div>
 
     </div>
 )
-
+}
 
 }
 export default Cocktails
