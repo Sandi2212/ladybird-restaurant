29ebe9e1fe7e5e3ca53ee16d630dd8271afe7110
Sandi <Sandiimayeguahi@gmail.com>
2019-02-17 01:03:18 -0500

    added class component and created axios call

--- src/components/Menus/Wine/Wine.js
+++ src/components/Menus/Wine/Wine.js
@@ -1,16 +1,42 @@
-import React from "react"
+import React , {Component} from "react"
+import axios from "axios"
 
-const Wine = () =>{
+class  Wine extends Component{
+    constructor(){
+        super()
+        this.state={
+            allWine:null,
+            allBeer:null
+        
+    }
+}
 
+getWineData = async () => {
+    await axios.get("http://localhost:5000/wines").then(response => {
+      const allWine = response.data;
+      this.setState({allWine});
+    });
+ }
+getBeerData = async () => {
+    await axios.get("http://localhost:5000/beers").then(response => {
+      const allBeer = response.data;
+      this.setState({allBeer});
+    });
+ }
+ componentDidMount(){
+    this.getWineData();
+    this.getBeerData();
+ }
 
-return(
+render(){
+    return(
 
 
     <div>
 
     </div>
 )
-
+}
 
 }
 export default Wine
