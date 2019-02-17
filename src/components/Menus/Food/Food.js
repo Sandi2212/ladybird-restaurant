70936d61a45854d8bc327f387bdcc6ffdc5d6814
Sandi <Sandiimayeguahi@gmail.com>
2019-02-17 01:04:49 -0500

    created food class component  with axios call

--- src/components/Menus/Food/Food.js
+++ src/components/Menus/Food/Food.js
@@ -1,17 +1,52 @@
-import React from "react"
-
-const Food = () =>{
-
- 
-
-return(
+import React , {Component} from "react"
+import axios from "axios"
+class  Food extends Component{
+constructor(){
+super()
+this.state={
+    allFood:null,
+    allFondues:null,
+    allDesserts:null
+    }
+}
+        getFonduesData = async () => {
+        await axios.get("http://localhost:5000/fondues").then(response => {
+          
+          const allFondues = response.data;
+          this.setState({allFondues});
+        });
+     }
+        getDessertsData = async () => {
+        await axios.get("http://localhost:5000/desserts").then(response => {
+       
+          const allDesserts = response.data;
+          this.setState({allDesserts});
+        });
+     }
+        getFoodData = async () => {
+        await axios.get("http://localhost:5000/foods").then(response => {
+        
+          const allFood = response.data;
+          this.setState({allFood});
+        });
+     }
+  
+     componentDidMount(){
+        this.getFonduesData();
+        this.getDessertsData();
+        this.getFoodData();
+     }
+  
+render(){
+    return(
 
 
     <div>
 
     </div>
 )
-
+}
 
 }
 export default Food
+   
