a8dad6241b627cdf41537cff9e28d73be71640ba
Sandi <Sandiimayeguahi@gmail.com>
2019-02-17 01:03:59 -0500

    imported food/wine/cocktails Component

--- src/components/Menus/Menus.js
+++ src/components/Menus/Menus.js
@@ -1,42 +1,21 @@
 import React, {Component} from 'react';
 import axios from "axios"
+import Food from "./Food/Food"
+import Wine from "./Wine/Wine"
+import Cocktails from "./Cocktails/Cocktails"
 import './Menus.css'
 
 class Menus extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            allFood : null,
-            allWine: null,
-            allCocktails:null
+           food:null,
+           cocktails:null,
+           wine:null
+
         }
     }
 
-    getWineData = async () => {
-        await axios.get("http://localhost:5000/wines").then(response => {
-          const allWine = response.data;
-          this.setState({allWine});
-        });
-     }
-    getCocktailsData = async () => {
-        await axios.get("http://localhost:5000/cocktails").then(response => {
-            console.log (response.data)
-          const allCocktails = response.data;
-          this.setState({allCocktails});
-        });
-     }
-    getFoodData = async () => {
-        await axios.get("http://localhost:5000/foods").then(response => {
-          const allFood = response.data;
-          this.setState({allFood});
-        });
-     }
-  
-     componentDidMount(){
-        this.getWineData();
-        this.getCocktailsData();
-        this.getFoodData();
-     }
   
         render() {
 
@@ -47,8 +26,11 @@ class Menus extends Component {
         
         <div className="menus_titles">
         <h1 className="menus_food">Food</h1>
+       
         <h1 className="menus_cocktails">Cocktails</h1>
+        
         <h1 className="menus_wine">Wine</h1>
+        
     
         </div>
         </div>
