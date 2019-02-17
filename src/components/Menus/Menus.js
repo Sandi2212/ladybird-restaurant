36b656ab53dc31d1a63f08efcbc64abad1f3bd23
Brian Ogilvie <brian@brianogilviedesigns.com>
2019-02-17 07:57:53 -0500

    conditionally render the sub menus

--- src/components/Menus/Menus.js
+++ src/components/Menus/Menus.js
@@ -12,19 +12,28 @@ class Menus extends Component {
         }
     }
 
-  
     render() {
-        return (
-            <div className="Menus">
-                <h1 className="menus_header">MENUS</h1>
-                <div className="menus_titles">
-                    <h1 className="menus_food">Food</h1>
-                    <h1 className="menus_cocktails">Cocktails</h1>
-                    <h1 className="menus_wine">Wine</h1>
-                </div>
-                <Food />
-            </div>
-        )
+      const activeMenu = this.state.activeMenu
+      const renderActiveMenue = (selectedMenu) => {
+        if (selectedMenu === 'food') {
+          return <Food />
+        } else if (selectedMenu === 'wine') {
+          return <Wine />
+        } else {
+          return <Cocktails />
+        }
+      }
+      return (
+          <div className="Menus">
+              <h1 className="menus_header">MENUS</h1>
+              <div className="menus_titles">
+                  <h1 className="menus_food">Food</h1>
+                  <h1 className="menus_cocktails">Cocktails</h1>
+                  <h1 className="menus_wine">Wine</h1>
+              </div>
+              {renderActiveMenue(activeMenu)}
+          </div>
+      )
     }
 }
 
