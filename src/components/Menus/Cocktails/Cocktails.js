8fdd62ab46be1d6efa7a9d08ed33af1b211a6d87
Sandi <Sandiimayeguahi@gmail.com>
2019-02-16 23:38:32 -0500

    added basic skeleton code to empty component

---
+++ src/components/Menus/Cocktails/Cocktails.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Cocktails= () =>{
+
+
+return(
+
+
+    <div>
+
+    </div>
+)
+
+
+}
+export default Cocktails
---
+++ src/components/Menus/Wine/Wine.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Wine = () =>{
+
+
+return(
+
+
+    <div>
+
+    </div>
+)
+
+
+}
+export default Wine
