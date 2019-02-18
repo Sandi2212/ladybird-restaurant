b58b8aa58dc8211f408b5a42d5a8a00c3e972523
Brian Ogilvie <brian@brianogilviedesigns.com>
2019-02-18 15:12:58 -0500

    set ReserveButton default text for responsive design

--- src/components/ReserveButton/ReserveButton.js
+++ src/components/ReserveButton/ReserveButton.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './ReserveButton.css'
 
 const ReserveButton = (props) => {
-  const buttonText = props.text || "Book Reservation"
+  const defaultText = window.innerWidth <= 500 ? "Reserve" : "Book Reservation"
+  const buttonText = props.text || defaultText
   return (
     <div className="ReserveButton__wrapper">
       <a className="ReserveButton" href="https://resy.com/cities/ny/ladybird?gclid=EAIaIQobChMI9577u6K-4AIVGI7ICh3ljgTsEAAYASAAEgLPr_D_BwE&seats=2&date=2019-02-15" target="_blank" rel="noopener noreferrer">
