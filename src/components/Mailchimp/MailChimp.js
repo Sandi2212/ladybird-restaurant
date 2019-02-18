311a5807cf72c815f8cdda26bfa4f823d258e1c0
Brian Ogilvie <brian@brianogilviedesigns.com>
2019-02-18 17:22:01 -0500

    responsive code for Footer and MailChimp

--- src/components/Mailchimp/MailChimp.js
+++ src/components/Mailchimp/MailChimp.js
@@ -5,42 +5,36 @@ class MailChimp extends React.Component {
   constructor() {
     super();
     this.state = {
-    emailValue: ''
-   
-};
-}
+      emailValue: ''
+    };
+  }
 
- render() {
+  render() {
     return (
-      <div className="parent">
-      
+      <div className="mailchimp__parent">
         <form action="https://mica.us20.list-manage.com/subscribe/post" method="POST">
-        <input type="hidden" name="u" value="b766b2cbbd7c7f8ef0c6ddf71"/>
-        <input type="hidden" name="id" value="69e75b390a"/>
-              <div className="Join-the-Mailing-List">
-                      Join the Mailing List
-              </div>
-              <label htmlFor='MERGE0'>
-                  
-                  <input 
-                      className="Email-Address"
-                      type="email" 
-                      placeholder="Email Address" 
-                      id="MERGE0"
-
-                      value={this.state.emailValue} 
-                      onChange={ (e)=>{this.setState({emailValue: e.target.value});} } 
-                      autoCapitalize="off" 
-                      autoCorrect="off"
-                    /> 
-              </label>
-              
-        <input type="submit" value="Sign Up" name="Sign Up" id="mc-embedded-subscribe" className="Sign-Up"/>
-      
-      </form>
-      </div>
-        )
-    }
+          <input type="hidden" name="u" value="b766b2cbbd7c7f8ef0c6ddf71"/>
+          <input type="hidden" name="id" value="69e75b390a"/>
+          <div className="Join-the-Mailing-List">
+            Join the Mailing List
+          </div>
+          <div className="mailchimp__input-wrapper">
+            <input 
+              className="Email-Address"
+              type="email" 
+              placeholder="Email Address" 
+              id="MERGE0"
+              value={this.state.emailValue} 
+              onChange={ (e)=>{this.setState({emailValue: e.target.value});} } 
+              autoCapitalize="off" 
+              autoCorrect="off"
+            /> 
+            <input type="submit" value="Sign Up" name="Sign Up" id="mc-embedded-subscribe" className="Sign-Up"/>
+          </div>
+        </form>
+        </div>
+    )
+  }
 }
 
 export default MailChimp
