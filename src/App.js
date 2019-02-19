93d49e4bd60fa57b20d38b782a1e42048200e41e
OneilCampbell <ocampbell@colgate.edu>
2019-02-18 21:48:12 -0500

    implemented logic in app to check if chef is logged in

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
 import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import ChefLogin from "./components/ChefLogin/ChefLogin";
+import Axios from "axios";
 
 class App extends Component {
     state = {
-        headerOffset: ""
+        headerOffset: "",
+        chefLoggedOn: false
     };
 
     getOffsetHeight = height => {
         this.setState({ headerOffset: height });
-        // let { offsetHeight } = this.refs.header;
-        // console.log(offsetHeight);
     };
+
+    isChefLoggedOn = async () => {
+        const respnse = await Axios.get("/auth/current_user");
+        const chefLoggedOn = respnse.data && true;
+        this.setState({chefLoggedOn});
+    }
+
+    componentDidMount() {
+        this.isChefLoggedOn()
+    }
 
     render() {
         return (
-            <div className="App">
-                <Header getOffsetHeight={this.getOffsetHeight} />
-                <Main headerOffset={this.state.headerOffset} />
-                <Footer />
-            </div>
+          <div className="App">
+            <Header getOffsetHeight={this.getOffsetHeight} />
+            <Switch>
+                <Route exact path="/" render={() => <Main {...this.state} />}/>
+                <Route path="/chef-login" render={() => <ChefLogin headerOffset={this.state.headerOffset} />} />
+            </Switch>
+            <Footer />
+          </div>
         );
     }
 }
